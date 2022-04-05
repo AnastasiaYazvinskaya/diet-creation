d40b59ad2b1bd4381870c108432f395e6062b220
AnastasiaYazvinskaya <kudasovaa@list.ru>
2022-04-06 01:39:45 +0300

    sqlalchemy connect

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_jsglue import JSGlue
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 from flask_mail import Mail
 
 app = Flask(__name__)
 jsglue = JSGlue(app)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 from flask_login import LoginManager
 login_manager = LoginManager(app)
--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import os
 
-#basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'database.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
